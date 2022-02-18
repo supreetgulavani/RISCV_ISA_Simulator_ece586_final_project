57dbde22fdbaf9f9f7a54961e286a02c4f86a7b1
Supreet Gulavani <sgsupreetgulavani@gmail.com>
2022-02-18 00:07:58 -0800

    request: cout operator overload

--- include/request.h
+++ include/request.h
@@ -9,22 +9,21 @@
 // Class for the request queue
 class request {
 public:
-    //program counter and address
-    uint64_t program_counter;
-    uint64_t address;
-
+    //Instruction Location, instruction
+    uint64_t instruction_location;
+    uint64_t instruction;
+    
+    //uint64_t program_counter;
     bool busy = false;
     bool valid = false;
-    
-   //prints program counter and address of the corresponding request
+
+   //prints instruction of the corresponding request
     friend std::ostream& operator<<(std::ostream& os, request &req)
     {
-        os << "Program Counter " << std::hex << req.program_counter << " " << "Address: " << std::uppercase << std::hex 
-            << req.address << std::endl;
-
+        os << "\nInstruction Location: " << std::uppercase << std::hex << req.instruction_location << "  " << "Instruction: " << std::uppercase << std::hex 
+            << req.instruction << "\n"  << std::endl;
         return os;
     }
-    
 };
 
 #endif
